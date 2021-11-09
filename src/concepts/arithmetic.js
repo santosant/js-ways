09143a1d5f0adf83b3adba6d205524030cfbd484
Antonio Santos <hiroto.anderson@gmail.com>
2021-11-09 19:37:40 -0300

    relationArithmatics

---
+++ src/concepts/arithmetic.js
@@ -0,0 +1,12 @@
+const [a, b, c, d] = [5, 10, 15, 20];
+
+const sum = a + b + c + d;
+const substr = d - b;
+const mult = a * b;
+const modulo = a % 2;
+
+console.log(sum, substr, mult, modulo);
+//tipos de operandos
+
+//-a operador binário que torna a variável negativa
+//
